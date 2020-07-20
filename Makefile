a09d2949ae32756b6ae27a5dc8cb7cb3f8704c88
xujing <xujing113221@hotmail.com>
2020-07-20 19:15:03 +0200

    fix make.sh

--- Makefile
+++ Makefile
@@ -4,7 +4,13 @@ COVERDE_OUT=JingXu_Anschreiben_2020
 RESUMEDE_IN=resume_de
 RESUMEDE_OUT=JingXu_Lebenslauf_2020
 
-RESUMEEN=resume_en
+COVEREN_IN=coverletter_en
+COVEREN_OUT=JingXu_Motivation_2020
+
+RESUMEEN_IN=resume_en
+RESUMEEN_OUT=JingXu_CV_2020
+
+OUTPUT="output/"
 
 .PHONY: cover_de resume_de clean depclean 
 
@@ -15,7 +21,7 @@ cover_de : $(COVERDE_OUT).pdf
 
 $(COVERDE_OUT).pdf: $(COVERDE_IN).tex 
 	lualatex -synctex=1 -interaction=nonstopmode $(COVERDE_IN).tex
-	mv $(COVERDE_IN).pdf $(COVERDE_OUT).pdf
+	mv $(COVERDE_IN).pdf $(OUTPUT)$(COVERDE_OUT).pdf
 
 #-----------------------------------------------------------
 # resume_de
@@ -24,12 +30,34 @@ resume_de : $(RESUMEDE_OUT).pdf
 
 $(RESUMEDE_OUT).pdf: $(COVERDE_IN).tex
 	lualatex -synctex=1 -interaction=nonstopmode $(RESUMEDE_IN).tex
-	mv $(RESUMEDE_IN).pdf $(RESUMEDE_OUT).pdf
+	mv $(RESUMEDE_IN).pdf $(OUTPUT)$(RESUMEDE_OUT).pdf
+
+#-----------------------------------------------------------
+# cover_en
+#-----------------------------------------------------------
+cover_en : $(COVEREN_OUT).pdf
+
+$(COVEREN_OUT).pdf: $(COVEREN_IN).tex 
+	lualatex -synctex=1 -interaction=nonstopmode $(COVEREN_IN).tex
+	mv $(COVEREN_IN).pdf $(OUTPUT)$(COVEREN_OUT).pdf
+
+#-----------------------------------------------------------
+# resume_en
+#-----------------------------------------------------------
+resume_en : $(RESUMEEN_OUT).pdf
+
+$(RESUMEEN_OUT).pdf: $(COVEREN_IN).tex
+	lualatex -synctex=1 -interaction=nonstopmode $(RESUMEEN_IN).tex
+	mv $(RESUMEEN_IN).pdf $(OUTPUT)$(RESUMEEN_OUT).pdf
 
 #-----------------------------------------------------------
 # resume
 #-----------------------------------------------------------
 cover_resume_de:  cover_de resume_de clean
+
+cover_resume_en:  cover_en resume_en clean
+
+all: cover_de resume_de cover_en resume_en clean
 
 
 #-----------------------------------------------------------
--- make.sh
+++ make.sh
@@ -3,20 +3,28 @@
 clear
 
 cover_in_de="coverletter_de"
-cover_out_de="JingXu_Anschreiben_2020.pdf"
+cover_out_de="output/JingXu_Anschreiben_2020.pdf"
 
 resume_in_de="resume_de"
-resume_out_de="JingXu_Lebenslauf_2020.pdf"
+resume_out_de="output/JingXu_Lebenslauf_2020.pdf"
 
-job_name=
-cmd=
+cover_in_en="coverletter_en"
+cover_out_en="output/JingXu_Motivation_2020.pdf"
+
+resume_in_en="resume_en"
+resume_out_en="output/JingXu_CV_2020.pdf"
+
+coverCmd_de=
+coverCmd_en=
 resumeCmd=
 
-getInfo(){
+function getInfo(){
     # clear
-    cmd=""
-    job_name=""
+    coverCmd_de=""
+    coverCmd_en=""
     resumeCmd=""
+
+    local cmd=""
 
     echo "=== Please tell me the name of you applicated Job:"
     read job_name
@@ -29,22 +37,24 @@ getInfo(){
     read -p "Postcode, location: " loc
     echo ""
 
-    
     echo "=== Please Choose the following options to tell me the Recipient:"
-    echo -e "\t1. Sehr geehrte Damen und Herren,"
-    echo -e "\t2. Sehr geehrter Herr Familienname," 
-    echo -e "\t3. Sehr geehrte Frau Familienname,"
+    echo -e "\t1. Dear Sir or Madam,"
+    echo -e "\t2. Dear Mr. Family Name," 
+    echo -e "\t3. Dear Ms. Family Name,"
     read -p "Input your choice nummber: " num
 
-    lettertitle="Sehr geehrte Damen und Herren,"
+    local letterbegin_de="Sehr geehrte Damen und Herren,"
+    local letterbegin_en="Dear Sir or Madam,"
     if [ $num -eq 2 ]
     then
-    read -p "Tell me the name: " hrname
-    lettertitle="Sehr geehrter Herr $hrname,"
+        read -p "Tell me the name: " hrname
+        letterbegin_de="Sehr geehrter Herr $hrname,"
+        letterbegin_en="Dear Mr. $hrname,"
     elif [ $num -eq 3 ]
     then
-    read -p "Tell me the name: " hrname
-    lettertitle="Sehr geehrte Frau $hrname,"
+        read -p "Tell me the name: " hrname
+        letterbegin_de="Sehr geehrte Frau $hrname,"
+        letterbegin_en="Dear Ms. $hrname"
     fi
 
     echo ""
@@ -54,8 +64,6 @@ getInfo(){
         cmd="\newcommand{\applicatename}{${job_name}} ${cmd}"
         resumeCmd="\newcommand{\applicatename}{${job_name}}"
     fi
-
-    cmd="\newcommand{\letterbegin}{${lettertitle}} ${cmd}"
 
     if [ -n "$recname" ]
     then
@@ -76,14 +84,12 @@ getInfo(){
     then 
     cmd="\newcommand{\companylocal}{${loc}} ${cmd}"
     fi
+
+    coverCmd_de="\newcommand{\letterbegin}{${letterbegin_de}} ${cmd}"
+    coverCmd_en="\newcommand{\letterbegin}{${letterbegin_en}} ${cmd}"
 }
 
-# Praktikant*in Embedded Softwareentwicklung für Brandmeldesysteme
-#  Bosch Gruppe
-# Grasbrunn bei München
-
-
-start(){
+function check(){
     getInfo
     echo "=== Please check your inputs!"
     read -p "If wrong, input 1. If right, input any key: " flag
@@ -91,21 +97,56 @@ start(){
 
     if [ $flag -eq 1 ]
     then
+        check
+    fi
+}
+
+function compile(){
+    coverCmd_de="${coverCmd_de} \input{${cover_in_de}}"
+    coverCmd_en="${coverCmd_en} \input{${cover_in_en}}"
+    local resumeCmd_de="${resumeCmd} \input{${resume_in_de}}"
+    local resumeCmd_en="${resumeCmd} \input{${resume_in_en}}"
+
+    if [ $1 -eq 1 ]     # create english resume and cover letter
+    then 
+        lualatex -synctex=1 -interaction=nonstopmode $resumeCmd_en
+        mv "${resume_in_en}.pdf" $resume_out_en
+        lualatex -synctex=1 -interaction=nonstopmode $coverCmd_en
+        mv "${cover_in_en}.pdf" $cover_out_en
+    elif [ $1 -eq 2 ]   # create german resume and cover letter
+    then
+        lualatex -synctex=1 -interaction=nonstopmode $resumeCmd_de
+        mv "${resume_in_de}.pdf" $resume_out_de
+        lualatex -synctex=1 -interaction=nonstopmode $coverCmd_de
+        mv "${cover_in_de}.pdf" $cover_out_de
+    else                # create both english and german
+        lualatex -synctex=1 -interaction=nonstopmode $resumeCmd_en
+        mv "${resume_in_en}.pdf" $resume_out_en
+        lualatex -synctex=1 -interaction=nonstopmode $resumeCmd_de
+        mv "${resume_in_de}.pdf" $resume_out_de
+        lualatex -synctex=1 -interaction=nonstopmode $coverCmd_en
+        mv "${cover_in_en}.pdf" $cover_out_en
+        lualatex -synctex=1 -interaction=nonstopmode $coverCmd_de
+        mv "${cover_in_de}.pdf" $cover_out_de
+    fi
+}
+
+function start(){
+    echo "=== Please choice language: "
+    echo -e "\t1.English\t2.German\t3.Both"
+    read lan
+
+    if [[ $lan -ne 1 && $lan -ne 2 && $lan -ne 3 ]]
+    then 
         start
+    else
+        check
+        compile $lan
+        test $lan
     fi
 }
 
 start
-coverCmd="${cmd} \input{${cover_in_de}}"
-resumeCmd="${resumeCmd} \input{${resume_in_de}}"
-
-lualatex -synctex=1 -interaction=nonstopmode $coverCmd
-mv "${cover_in_de}.pdf" $cover_out_de
-
-lualatex -synctex=1 -interaction=nonstopmode $resumeCmd
-mv "${resume_in_de}.pdf" $resume_out_de
-
 make clean
-
 
 
